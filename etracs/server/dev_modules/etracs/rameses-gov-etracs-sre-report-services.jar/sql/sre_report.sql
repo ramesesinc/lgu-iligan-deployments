fd9a3ab9b37afd988377555a5a9ef82b662499a0
worgflores <worgflores@gmail.com>
2019-12-11 22:10:32 +0800

    updated srs report

--- etracs/server/dev_modules/etracs/rameses-gov-etracs-sre-report-services.jar/sql/sre_report.sql
+++ etracs/server/dev_modules/etracs/rameses-gov-etracs-sre-report-services.jar/sql/sre_report.sql
@@ -84,16 +84,16 @@ from (
 	select 
 		a.objid, a.code, a.title, a.type, 
 		a.leftindex, a.rightindex, a.level, 
-		a.maingroupid, null as amount, sum(ait.target) as target 
-	from account a 
-		inner join account_item_mapping aim on aim.acctid = a.objid 
-		inner join account_incometarget ait on ait.itemid = aim.itemid 
-	where a.maingroupid = $P{maingroupid} 
-		and ait.year = YEAR($P{startdate}) 
+		a.maingroupid, null as amount, 
+		sum(t.target) as target 
+	from account_item_mapping aim 
+		inner join account_incometarget t on t.itemid = aim.acctid 
+		inner join account a on a.objid = aim.acctid
+	where aim.maingroupid = $P{maingroupid} 
+		and t.year = YEAR($P{startdate}) 
 	group by 
 		a.objid, a.code, a.title, a.type, 
-		a.leftindex, a.rightindex, a.level, 
-		a.maingroupid 		
+		a.leftindex, a.rightindex, a.level, a.maingroupid
 )t1 
 group by 
 	t1.objid, t1.code, t1.title, t1.type, 
@@ -163,16 +163,16 @@ from (
 	select 
 		a.objid, a.code, a.title, a.type, 
 		a.leftindex, a.rightindex, a.level, 
-		a.maingroupid, null as amount, sum(ait.target) as target 
-	from account a 
-		inner join account_item_mapping aim on aim.acctid = a.objid 
-		inner join account_incometarget ait on ait.itemid = aim.itemid 
-	where a.maingroupid = $P{maingroupid} 
-		and ait.year = YEAR($P{startdate}) 
+		a.maingroupid, null as amount, 
+		sum(t.target) as target 
+	from account_item_mapping aim 
+		inner join account_incometarget t on t.itemid = aim.acctid 
+		inner join account a on a.objid = aim.acctid
+	where aim.maingroupid = $P{maingroupid} 
+		and t.year = YEAR($P{startdate}) 
 	group by 
 		a.objid, a.code, a.title, a.type, 
-		a.leftindex, a.rightindex, a.level, 
-		a.maingroupid 		
+		a.leftindex, a.rightindex, a.level, a.maingroupid  		
 )t1 
 group by 
 	t1.objid, t1.code, t1.title, t1.type, 
